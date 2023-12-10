231a8950b315b81824f586628dd0e49e2184333d
petebannister <pete.bannister@gems.co.uk>
2023-12-10 13:28:55 +0000

    Tidy up.

--- day10/main.cpp
+++ day10/main.cpp
@@ -11,37 +11,10 @@ constexpr uint8_t U = 0;
 constexpr uint8_t R = 1;
 constexpr uint8_t D = 2;
 constexpr uint8_t L = 3;
-Point INVALID = { 0, 0 };
+constexpr Point INVALID = { 0, 0 };
 
-Point go(Point dir, char c) {
-    if (dir == DIRS[U]) {
-        if (c == '|') {
-            return dir;
-        }
-        return (c == 'F') ? DIRS[R] : (c == '7') ? DIRS[L] : INVALID;
-    }
-    if (dir == DIRS[R]) {
-        if (c == '-') {
-            return dir;
-        }
-        return (c == '7') ? DIRS[D] : (c == 'J') ? DIRS[U] : INVALID;
-    }
-    if (dir == DIRS[D]) {
-        if (c == '|') {
-			return dir;
-		}
-		return (c == 'J') ? DIRS[L] : (c == 'L') ? DIRS[R] : INVALID;
-	}
-    if (dir == DIRS[L]) {
-        if (c == '-') {
-			return dir;
-		}
-		return (c == 'L') ? DIRS[U] : (c == 'F') ? DIRS[D] : INVALID;
-	}
-    return INVALID;
-}
-
-int angle(Point dir, char c) {
+//-----------------------------------------------------------------------------
+constexpr int turn(Point dir, char c) {
     if (dir == DIRS[U]) {
         if (c == '|') {
             return 0;
@@ -68,27 +41,40 @@ int angle(Point dir, char c) {
     }
     return 0;
 }
+//-----------------------------------------------------------------------------
+Point turn_dir(Point dir, int t) {
+    if (t < 0) {
+        return dir.ccw90();
+    }
+    if (t > 0) {
+        return dir.cw90();
+    }
+    return dir;
+
+}
+//-----------------------------------------------------------------------------
+Point go(Point dir, int c) {
+    auto t = turn(dir, c);
+    return turn_dir(dir, t);
+}
 
 //-----------------------------------------------------------------------------
 void Solve() {
     Input i;
 
-    uint32_t part1 = 0u;
-    uint32_t part2 = 0u;
-
     auto lines = i.lines();
     vector<string_view> maze(lines.begin(), lines.end());
     unordered_set<Point> loop_points;
 
+    // Get a symbol from the maze.
     auto get = [&](Point p) {
-        if (p.y < 0 || p.y >= (int)maze.size()) {
-			return '.';
+        if (p.y >= 0 || p.y < (int)maze.size()) {
+		    auto& row = maze[p.y];
+            if (p.x > 0 || p.x < (int)row.size()) {
+                return row[p.x];
+            }
 		}
-		auto& row = maze[p.y];
-        if (p.x < 0 || p.x >= (int)row.size()) {
-			return '.';
-		}
-		return row[p.x];
+		return '.';
 	};
 
     Point S;
@@ -103,7 +89,6 @@ void Solve() {
     for (auto d : DIRS) {
         auto p = S + d;
         auto c = get(p);
-        println(c);
         auto dd = go(d, c);
         if (dd != INVALID) {
             initial_dir = dd;
@@ -114,38 +99,46 @@ void Solve() {
 
     auto d = initial_dir;
     auto p = S + initial_dir;
-    uint64_t steps = 0u;
+    uint32_t steps = 0u;
     int a = 0; // angle
     while (p != S) {
         auto c = get(p);
-        a += angle(d, c); // must be before go()
-        d = go(d, c);
+        auto t = turn(d, c);
+        a += t;
+        d = turn_dir(d, t);
         p += d;
         ++steps;
         loop_points.insert(p);
     }
-    assert(steps % 2 != 0);
-    assert(a == 4 || a == -4);
-    part1 = (steps / 2) + 1;
+    assert(steps % 2 != 0); // Should be odd for a unique solution.
+    assert(a == 4 || a == -4); // Should be a closed loop in either CW or CCW direction.
 
+    auto part1 = (steps / 2) + 1; // since steps is odd + 1.
     println("part1: ", part1); // 7086
 
+    // Part 2
+
+    // Get first set of points in the interior of the loop, to 
+    // seed the fill algorithm.
     unordered_set<Point> interior;
-    bool ccw = a < 0;
+    bool const ccw = a < 0; // Determine which side of the loop line to fill.
     d = initial_dir;
     p = S + initial_dir;
     while (p != S) {
         auto c = get(p);
-        //if (c == '|' || c == '-') {
-            auto dd = (ccw) ? d.ccw90() : d.cw90();
-            auto p_int = (p + dd);
-            if (!loop_points.contains(p_int)) {
-				interior.insert(p_int);
-			}
-        //}
+        auto dd = (ccw) ? d.ccw90() : d.cw90();
+        auto p_int = (p + dd);
+        if (!loop_points.contains(p_int)) {
+			interior.insert(p_int);
+		}
         d = go(d, c);
         p += d;
     }
+    // Fill the interior.  Put all known starting points into a queue.
+    // For each point see if there is an adjacent point that we have not
+    // identified as interior yet.  If so, add it to the interior provided
+    // it is not part of the loop.  This point gets added to the queue so
+    // that the whole interior gets filled.
     vector<Point> q(interior.begin(), interior.end());
     while (!q.empty()) {
 		auto p = q.back();
@@ -160,27 +153,8 @@ void Solve() {
 			}
         }
     }
-    part2 = interior.size();
-
-    for (auto y : integers(maze.size())) {
-		auto& row = maze[y];
-        for (auto x : integers(row.size())) {
-			auto p = Point{ (int)x, (int)y };
-            if (interior.contains(p)) {
-				print('o');
-			}
-            else if (loop_points.contains(p)) {
-                print(row[x]);
-            }
-            else { 
-                print('.'); 
-            }
-		}
-        print('\n');
-	}
-    
-    // 268 too low.
-    println("part2: ", part2);
+    auto part2 = interior.size();
+    println("part2: ", part2); // 317
 }
 
 //-----------------------------------------------------------------------------
