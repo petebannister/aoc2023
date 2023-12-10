c12b2e39e977f4b5301a89e98cb5672bc4a62c28
petebannister <pete.bannister@gems.co.uk>
2023-12-10 13:09:37 +0000

    part2: 35:53

--- day10/main.cpp
+++ day10/main.cpp
@@ -41,6 +41,34 @@ Point go(Point dir, char c) {
     return INVALID;
 }
 
+int angle(Point dir, char c) {
+    if (dir == DIRS[U]) {
+        if (c == '|') {
+            return 0;
+        }
+        return (c == 'F') ? 1 : -1;
+    }
+    if (dir == DIRS[R]) {
+        if (c == '-') {
+            return 0;
+        }
+        return (c == '7') ? 1 : -1;
+    }
+    if (dir == DIRS[D]) {
+        if (c == '|') {
+            return 0;
+        }
+        return (c == 'J') ? 1 : -1;
+    }
+    if (dir == DIRS[L]) {
+        if (c == '-') {
+            return 0;
+        }
+        return (c == 'L') ? 1 : -1;
+    }
+    return 0;
+}
+
 //-----------------------------------------------------------------------------
 void Solve() {
     Input i;
@@ -50,6 +78,7 @@ void Solve() {
 
     auto lines = i.lines();
     vector<string_view> maze(lines.begin(), lines.end());
+    unordered_set<Point> loop_points;
 
     auto get = [&](Point p) {
         if (p.y < 0 || p.y >= (int)maze.size()) {
@@ -86,18 +115,71 @@ void Solve() {
     auto d = initial_dir;
     auto p = S + initial_dir;
     uint64_t steps = 0u;
+    int a = 0; // angle
     while (p != S) {
         auto c = get(p);
+        a += angle(d, c); // must be before go()
         d = go(d, c);
         p += d;
         ++steps;
+        loop_points.insert(p);
     }
     assert(steps % 2 != 0);
+    assert(a == 4 || a == -4);
     part1 = (steps / 2) + 1;
+
+    println("part1: ", part1); // 7086
+
+    unordered_set<Point> interior;
+    bool ccw = a < 0;
+    d = initial_dir;
+    p = S + initial_dir;
+    while (p != S) {
+        auto c = get(p);
+        //if (c == '|' || c == '-') {
+            auto dd = (ccw) ? d.ccw90() : d.cw90();
+            auto p_int = (p + dd);
+            if (!loop_points.contains(p_int)) {
+				interior.insert(p_int);
+			}
+        //}
+        d = go(d, c);
+        p += d;
+    }
+    vector<Point> q(interior.begin(), interior.end());
+    while (!q.empty()) {
+		auto p = q.back();
+		q.pop_back();
+        for (auto d : DIRS) {
+			auto p2 = p + d;
+            if (!interior.contains(p2)) {
+                if (!loop_points.contains(p2)) {
+                    interior.insert(p2);
+                    q.push_back(p2);
+                }
+			}
+        }
+    }
+    part2 = interior.size();
+
+    for (auto y : integers(maze.size())) {
+		auto& row = maze[y];
+        for (auto x : integers(row.size())) {
+			auto p = Point{ (int)x, (int)y };
+            if (interior.contains(p)) {
+				print('o');
+			}
+            else if (loop_points.contains(p)) {
+                print(row[x]);
+            }
+            else { 
+                print('.'); 
+            }
+		}
+        print('\n');
+	}
     
-
-
-    println("part1: ", part1);
+    // 268 too low.
     println("part2: ", part2);
 }
 
