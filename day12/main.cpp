b41285d7b436b962eb84cdae2c3c96bf030e4294
petebannister <pete.bannister@gems.co.uk>
2023-12-13 11:02:41 +0000

    part2

--- day12/main.cpp
+++ day12/main.cpp
@@ -1,117 +1,161 @@
 #include "aoc.h"
 
 //-----------------------------------------------------------------------------
-void Solve() {
-    Input i; // ("example.txt");
+struct State
+{
 
-    uint32_t part1 = 0u;
-    uint32_t part2 = 0u;
+    char ch = '?';
+    uint8_t i = 0u;
+    uint8_t wi = 0u;
+    int8_t w = -1;
 
-    auto printbits = [](uint32_t v, uint8_t nbits) {
-		while (nbits--) {
-			print((v & 1) ? "#" : ".");
-			v >>= 1;
-		}
-        println();
-	};
-
-    auto valid = [](uint32_t _v, auto const& widths, uint32_t set_bits, uint32_t unset_bits) {
-        auto v = _v;
-        if ((v & set_bits) != set_bits) {
-            return false;
-        }
-        if ((v & unset_bits) != 0) {
-			return false;
-		}
-        for (auto w : widths) {
-            while ((v & 1) == 0) {
-                v >>= 1;
-                if (v == 0) {
-					return false;
-				}
-            }
-            while (w--) {
-                if ((v & 1) == 0) {
-                    return false;
-                }
-                v >>= 1;
-            }
-            if ((v & 1) != 0) {
-                return false;
-			}
-        }
-        if (v != 0) {
-            return false;
-        }
-        return true;
-    };
-
-    for (Scanner line : i.lines()) {
-        println("---------");
-        println(line.line());
-
-
-        auto s = line.str(' ');
-        auto widths = line.vec<uint8_t>(',');
-        std::reverse(widths.begin(), widths.end()); // LSB first
-        uint32_t set_bits = 0u;
-        uint32_t unset_bits = 0u;
-        uint32_t unk = 0u; // unknown bits
-        uint8_t nbits = s.size();
-        assert(s.size() <= (sizeof(set_bits) * 8));
-        for (auto c : s) {
-            set_bits <<= 1;
-            unset_bits <<= 1;
-			unk <<= 1;
-
-            if (c == '?') {
-                unk |= 1u;
-            }
-            else if (c == '#') {
-                set_bits |= 1u;
-			}
-            else if (c == '.') {
-                unset_bits |= 1u;
-            }
-        }
-
-        uint32_t n = 0u;
-        for (auto v : integers(1 << nbits)) {
-            if (valid(v, widths, set_bits, unset_bits)) {
-				n++;
-			}
-		}
-        println(n, " ", s);
-        part1 += n;
-
-#if 0
-        vector<uint8_t> shifts;
-        uint8_t shift = 0u;
-        for (auto w : widths) {
-            shifts.push_back(shift);
-            shift += w + 1;
-        }
-        auto min_bits = std::accumulate(widths.begin(), widths.end(), 0u) + (widths.size() - 1);
-        auto spare_bits = (nbits - min_bits); // bits per shift
-        auto combinations = (nbits - min_bits) * widths.size();
-        for (auto sn : integers(shifts.size())) {
-            for (auto b : integers(spare_bits)) {
-
-            }
-        }
-        println(spare_bits, ", ", widths.size(), " ", s);
-#endif
+    auto value() const {
+        return (uint32_t((uint8_t)w) << 24) | (uint32_t(wi) << 16) | (uint32_t(i) << 8) | (uint32_t(ch));
     }
-
-    // 489370 too high, 103361 too high..
-    println("part1: ", part1);
-    println("part2: ", part2);
+    bool operator==(State const& rhs) const {
+		return value() == rhs.value();
+	}
+    bool operator!=(State const& rhs) const {
+		return value() != rhs.value();
+	}
+	bool operator<(State const& rhs) const {
+		return value() < rhs.value();
+	}
+};
+namespace std {
+	template<> struct hash<State> {
+		size_t operator()(State const& s) const {
+			return s.value();
+		}
+	};
 }
 
+struct X {
+    string_view s;
+    vector<uint8_t> W;
+    string dbg;
+    unordered_map<State, uint64_t> S;
+
+    uint64_t f() {
+        S.clear();
+		return f(0, 0, -1);
+	}
+    uint64_t fc(char ch, uint8_t i, uint8_t wi, int8_t w) {
+        auto st = State{ ch, i, wi, w };
+        auto it = S.find(st);
+        if (it != S.end()) {
+			return it->second;
+		}
+        auto n = fc_i(ch, i, wi, w);
+        S[st] = n;
+        return n;
+    }
+    uint64_t fc_i(char ch, uint8_t i, uint8_t wi, int8_t w) {
+        dbg[i] = ch;
+        //print_dbg(i);
+        if (ch == '#') {
+            // is # allowed here?
+            if (w == 0) {
+                // no - must be '.' or end of string
+				return 0u;
+			}
+            if (w > 0) {
+                // existing group
+				return f(i + 1, wi, w - 1);
+			}
+            // new group
+            if (wi == W.size()) {
+                // no more widths
+                return 0u;
+            }
+            return f(i + 1, wi + 1, W[wi] - 1);
+        }
+        else if (ch == '.') {
+            // is . allowed here?
+            if (w > 0) {
+                // no
+                return 0u;
+            }
+            return f(i + 1, wi, -1);
+        }
+        else {
+            assert(false);
+        }
+        return 0;
+    }
+    uint64_t f(uint8_t i, uint8_t wi, int8_t w) {
+        if (i == s.size()) {
+            if (w < 1) {
+                if (wi == W.size()) {
+                    return 1u;
+                }
+            }
+            return 0u;
+		}
+		uint64_t n = 0u;
+        auto ch = s[i];
+		if (ch == '?') {
+            n += fc('#', i, wi, w);
+            n += fc('.', i, wi, w);
+		}
+        else {
+            n += fc(ch, i, wi, w);
+        }
+		return n;
+    }
+    void print_dbg(uint8_t i) {
+        println(string_view(dbg.c_str(), i + 1));
+    }
+};
+
+//-----------------------------------------------------------------------------
+void Solve(bool part2) {
+    Input i; // ("example.txt");
+
+    uint64_t ans = 0u;
+    
+    vector<State> S;
+    string s;
+
+    X x;
+
+    for (Scanner line : i.lines()) {
+        auto s1 = line.str(' ');
+        auto _widths = line.vec<uint8_t>(',');
+                
+        s.clear();
+        vector<uint8_t> W;
+
+        uint8_t copies = (part2) ? 5 : 1; // 5 for part 2
+
+        for (auto i : integers(copies)) {
+            if (s.empty()) {
+                s.assign(s1.begin(), s1.end());
+            }
+            else {
+                s += '?';
+                s.append(s1.begin(), s1.end());
+            }
+            W.insert(W.end(), _widths.begin(), _widths.end());
+        }
+        println(s);
+        
+        x.s = s;
+        x.dbg = s;
+        x.W = W;
+        auto n = x.f();
+        ans += n;
+        println(n);
+    }
+
+    // part1: 7007
+    println((part2)?"part2: ": "part1: ", ans);
+}
 //-----------------------------------------------------------------------------
 int main() {
     try {
-        Solve();    
+        Solve(false); // 7007
+        Solve(true); // 3476169006222
     }
     catch (const exception& e) {
         printf("Exception: %s\n", e.what());
